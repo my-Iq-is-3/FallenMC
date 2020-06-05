9336f886fff01d85c4683ad6f6489e12253dc2f6
BuildTools <zachary2904@gmail.com>
2020-06-05 09:53:34 -0400

    Added Corrupter t1 ArrayList

--- src/me/zach/DesertMC/GameMechanics/ClassEvents/CorrupterEvents/EventsForCorruptor.java
+++ src/me/zach/DesertMC/GameMechanics/ClassEvents/CorrupterEvents/EventsForCorruptor.java
@@ -1,12 +1,23 @@
 package me.zach.DesertMC.GameMechanics.ClassEvents.CorrupterEvents;
 
+import me.zach.DesertMC.DesertMain;
+import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class EventsForCorruptor {
     public static final EventsForCorruptor INSTANCE = new EventsForCorruptor();
 
 
     public void t1Event(EntityDamageByEntityEvent event){
-        
+        PlayerInteractEvent event1;
+        if(event.getEntity() instanceof Player && event.getDamager() instanceof Player){
+            Player damaged = (Player) event.getEntity();
+            Player damager = (Player) event.getDamager();
+            if(damaged.getHealth() - event.getDamage() < 0.1){
+                DesertMain.ct1players.add(damager.getUniqueId());
+            }
+        }
+
     }
 }
